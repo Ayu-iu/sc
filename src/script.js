9915e7a17060c8ffb4ea17cb4bb3a41ba09c1c24
Ayu-iu <olgerah.ayantu@gmail.com>
2024-07-17 20:57:54 +0300

    final touchs

--- src/script.js
+++ src/script.js
@@ -1,23 +1,24 @@
 function newData(event) {
-  let time = new Date();
-  let day = [
-    "0",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
   event.preventDefault();
   let input = document.querySelector("#input-field");
   let newInput = document.querySelector("h1");
-  let currentTime = document.querySelector("#time");
   newInput.innerHTML = `${input.value}`;
-  currentTime.innerHTML = `${
-    day[time.getDay()]
-  } ${time.getHours()}:${time.getMinutes()},`;
 }
 let form = document.querySelector("#form-field");
 form.addEventListener("submit", newData);
+
+let time = new Date();
+let day = [
+  "null",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let currentTime = document.querySelector("#time");
+currentTime.innerHTML = `${
+  day[time.getDay()]
+} ${time.getHours()}:${time.getMinutes()},`;
